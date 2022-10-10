4b8c6a35fb5c6b0479aec1eefcf8cc9898381b0a
gkalyanprasannasaikumar <111508565+gkalyanprasannasaikumar@users.noreply.github.com>
2022-10-10 20:23:37 +0530

    Delete springapp directory

--- springapp/src/main/java/com/virtusa/abacusacademyadmissionportal/repository/CourseRepository.java
+++
@@ -1,9 +0,0 @@
-package com.virtusa.abacusacademyadmissionportal.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.virtusa.abacusacademyadmissionportal.model.CourseModel;
-
-public interface CourseRepository extends JpaRepository<CourseModel, Integer> {
-
-}
--- springapp/src/main/java/com/virtusa/abacusacademyadmissionportal/repository/InstituteRepository.java
+++
@@ -1,9 +0,0 @@
-package com.virtusa.abacusacademyadmissionportal.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.virtusa.abacusacademyadmissionportal.model.InstituteModel;
-
-public interface InstituteRepository extends JpaRepository<InstituteModel, Integer> {
-
-}
